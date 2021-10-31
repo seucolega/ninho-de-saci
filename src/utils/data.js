be5ecc91ccd1908f9371e658aa975d27e810b58b
Gustavo Sousa <ola@gustavosousa.me>
2021-10-31 15:01:34 -0300

    Changing text on About

--- src/utils/data.js
+++ src/utils/data.js
@@ -12,7 +12,7 @@ export const navbarContent = {
 
 export const aboutContent = {
   title: 'Sobre o Livro',
-  text: `O Ninho de Saci é livro digital escrita pelo artista Raoni Assis e ilustrada pelo mesmo, com bonecos, miudezas, um cenário feito por ele e fotografados em um estúdio caseiro. Essa estética remete a uma forma de brincar, uma pequena maquete, feita de forma totalmente original e pelas mãos do artista.  
-  Historia é um conto sobre uma menina que perde alguma coisa por conta da bagunça dentro dela, refletida em seu próprio quarto, mas consegue desdobrar dentro dessa bagunça as frustrações, agruras e o que é necessário para organizar-se, por dentro, no ambiente e para encontrar suas coisas e encontrar com ela mesma. Ela começa a questionar se as coisas têm mesmo um lugar certeiro e se ela, como as coisas, também pode pertencer a todos os lugares. Nisso, ela tem uma epifania de organização e pertencimento.
-  Ninho de saci, expressão popular que quer dizer lugar bagunçado, aqui é empregue com lugar de resistência, onde a menina se apropria da sua própria bagunça para encontrar o seu lugar.`,
+  text: 'O Ninho de Saci é um livro digital escrito pelo artista Raoni Assis e ilustrada pelo mesmo, com bonecos, miudezas, um cenário feito por ele e fotografados em um estúdio caseiro. Essa estética remete a uma forma de brincar, uma pequena maquete, feita de forma totalmente original e pelas mãos do artista.\n\n' +
+      'É um conto sobre uma menina que perde alguma coisa por conta da bagunça dentro dela, refletida em seu próprio quarto, mas consegue desdobrar dentro dessa bagunça as frustrações, agruras e o que é necessário para organizar-se, por dentro, no ambiente e para encontrar suas coisas e encontrar com ela mesma. Ela começa a questionar se as coisas têm mesmo um lugar certeiro e se ela, como as coisas, também pode pertencer a todos os lugares. Nisso, ela tem uma epifania de organização e pertencimento.\n\n' +
+      'Ninho de saci, expressão popular que quer dizer lugar bagunçado, aqui é empregue com lugar de resistência, onde a menina se apropria da sua própria bagunça para encontrar o seu lugar.',
 };
